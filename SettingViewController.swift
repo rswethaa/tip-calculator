04ff575b5825bb1939696d475aac3461d250e506
Swethaa Rajasekaran <rswethaa28@gmail.com>
2016-09-28 01:34:38 -0700

    First commit

---
+++ SettingViewController.swift
@@ -0,0 +1,89 @@
+//
+//  SettingViewController.swift
+//  TipCalculator
+//
+//  Created by Swethaa Rajasekaran on 9/25/16.
+//  Copyright © 2016 Swethaa. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * ViewController containing logic for all the settings changes
+ */
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var roundSwitch: UISwitch!
+    @IBOutlet weak var bgcolorSlider: UISlider!
+    
+    var userDefaults : NSUserDefaults?
+    let defaultTip: Double? = nil
+    var currentTip: Double? = nil
+    let tipRates = [0.18, 0.2, 0.25]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        userDefaults = NSUserDefaults.standardUserDefaults()
+        resetAllConfigToDefault()
+    }
+    
+    func resetAllConfigToDefault() {
+        resetTipToPersistedValue()
+        resetRoundingToPersistedValue()
+        resetBgColorSliderValue()
+    }
+    
+    @IBAction func defaultRateModified(sender: AnyObject) {
+        let currentTip = tipRates[tipControl.selectedSegmentIndex]
+        
+        userDefaults?.setDouble(currentTip, forKey: "defaultTip")
+        userDefaults?.synchronize()
+    }
+    
+    func resetTipToPersistedValue() {
+        let persistedTipValue = userDefaults?.doubleForKey("defaultTip")
+        print("DEBUG: Persisted value of tip : ", persistedTipValue)
+        
+        for (index,value) in tipRates.enumerate() {
+            if(persistedTipValue == value){
+                tipControl.selectedSegmentIndex = index
+            }
+        }
+    }
+    
+    @IBAction func roundingToggled(sender: AnyObject) {
+        userDefaults?.setBool(roundSwitch.on, forKey: "defaultRound")
+        userDefaults?.synchronize()
+    }
+    
+    func resetRoundingToPersistedValue() {
+        let persistedRoundingValue = userDefaults?.boolForKey("defaultRound")
+        if ((persistedRoundingValue) != nil && persistedRoundingValue == true) {
+            roundSwitch.setOn(true, animated: true)
+        } else {
+            roundSwitch.setOn(false, animated: true)
+        }
+    }
+    
+    @IBAction func colorSliderModified(sender: AnyObject) {
+        let sliderValue = bgcolorSlider.value
+        self.view.backgroundColor = UIColor(hue: CGFloat(sliderValue), saturation: 0.9, brightness: 1.0, alpha: 2.0)
+        userDefaults?.setFloat(sliderValue, forKey: "defaultBackgroundColorSliderValue")
+    }
+    
+    func resetBgColorSliderValue() {
+        let persistedBgColorSliderValue = userDefaults?.floatForKey("defaultBackgroundColorSliderValue")
+        if (persistedBgColorSliderValue != nil) {
+            bgcolorSlider.value = persistedBgColorSliderValue!;
+            self.view.backgroundColor = UIColor(hue: CGFloat(bgcolorSlider.value), saturation: 0.9, brightness: 1.0, alpha: 2.0)
+        }
+    }
+}
---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Swethaa Rajasekaran on 9/19/16.
+//  Copyright © 2016 Swethaa. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * Main view controller for the iOS application
+ */
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billText: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var userDefaults : NSUserDefaults?
+    var defaultTip : Double?
+    let tipRates = [0.18, 0.20, 0.25]
+    var roundingEnabled : Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        defaultTip = userDefaults?.doubleForKey("defaultTip")
+        for (index,tmp) in tipRates.enumerate() {
+            if(defaultTip == tmp){
+                tipControl.selectedSegmentIndex = index
+            }
+        }
+        
+        roundingEnabled = userDefaults?.boolForKey("defaultRound")
+        
+        let defaultBgColorSliderValue = userDefaults?.floatForKey("defaultBackgroundColorSliderValue")
+        if (defaultBgColorSliderValue != nil) {
+            let newBackgroundColor = UIColor(hue: CGFloat(defaultBgColorSliderValue!), saturation: 0.9, brightness: 1.0, alpha: 2.0)
+            self.view.backgroundColor = newBackgroundColor
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /**
+     * Hide keyboard on tapping anywhere on the app
+     */
+    @IBAction func OnTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        let bill = Double(billText.text!) ?? 0
+        let tip = bill * tipRates[tipControl.selectedSegmentIndex]
+        var total = tip + bill
+        if (roundingEnabled != nil && roundingEnabled == true)
+        {
+            total = round(total)
+        }
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+    
+}
+
+
+
